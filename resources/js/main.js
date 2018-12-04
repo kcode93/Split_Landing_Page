e321a1b72dc4109331bbb241f5447131c511d46c
Kenneth Garcia <kennethgarcia12@hotmail.com>
2018-12-03 23:20:00 -0800

    fixed index.html script tag and added media queries for responsiveness

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,19 +2,20 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const container = document.querySelector('.container');
 
-//add class hover-left when hovering hover
+//adds and removes class hover-left when hovering hover
 left.addEventListener('mouseenter', () => {
-  container.classList.add('hover-left');
+  container.classList.add("hover-left");
 });
 
 left.addEventListener('mouseleave', () => {
-  container.classList.remove('hover-left');
+  container.classList.remove("hover-left");
 });
 
+//adds and removes class hover-right when hovering hover
 right.addEventListener('mouseenter', () => {
-  container.classList.add('hover-right');
+  container.classList.add("hover-right");
 });
 
 right.addEventListener('mouseleave', () => {
-  container.classList.remove('hover-right');
+  container.classList.remove("hover-right");
 });
